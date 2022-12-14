54a73c32daffc0928e47c17396d15b53f92b99a3
EC2 Default User <amrumineq@gmail.com>
2022-12-14 09:42:59 +0000

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -46,8 +46,8 @@ Rails.application.routes.draw do
       resources:comments, only: [:destroy]
     end
     resources :tags, only: [:index,:destroy]
-     get "search" => "searches#search"
+    get "search" => "searches#search"
   end
-
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
